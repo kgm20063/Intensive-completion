9c3a6f168707aa05887c26d32f39d848e68d0481
kgm20063 <kgm20063@gmail.com>
2023-05-09 09:24:04 +0900

    FEAT: main index 추가

---
+++ 2023.05.08/react-study/src/pages/main/index.jsx
@@ -0,0 +1,16 @@
+import Button from "../../components/Button";
+
+import * as S from "./styled";
+
+function Main() {
+  return (
+    <S.Container>
+      <S.MainWapper>
+        <div>메인페이지</div>
+        <Button>만들기</Button>
+      </S.MainWapper>
+    </S.Container>
+  );
+}
+
+export default Main;
