6073d09319a16f3b6e07ed039803d07ea0d89c1e
kgm20063 <kgm20063@gmail.com>
2023-05-10 11:22:19 +0900

    FEAT: 커서 스타일 수정

--- 2023.05.08/react-study/src/pages/todos/CreateItemBox/styled.js
+++ 2023.05.08/react-study/src/pages/todos/CreateItemBox/styled.js
@@ -6,17 +6,23 @@ export const InputBox = styled.div`
   display: flex;
   gap: 4px;
   width: 100%;
-  ${Button} {
+  > ${Button} {
     flex-grow: 1;
     flex-shrink: 0;
-    padding: 4px 8px
+    padding: 10px 20px;
     border: none;
     cursor: pointer;
+    background: #ddd;
+    &:disabled {
+      cursor: not-allowed;
+    }
   }
 `;
 
 export const Input = styled.input`
   flex-shrink: 0;
   width: 100%;
-  padding: 4px;
+  padding: 10px 5px;
+  border: none;
+  box-shadow: 0 0 4.5px rgba(156, 156, 156, 04);
 `;
