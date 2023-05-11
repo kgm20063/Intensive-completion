d6d8c8cf9ed1165fe24dda8215dfb338ec6aa031
kgm20063 <kgm20063@gmail.com>
2023-05-11 10:28:06 +0900

    FEAT: todos 저장 기능 추가

--- 2023.05.08/react-study/src/pages/todos/index.jsx
+++ 2023.05.08/react-study/src/pages/todos/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import * as S from "./styled";
@@ -13,18 +13,40 @@ function Todos() {
   const createTodo = () => {
     // if (!todoName.trim()) return;
     setTodoName("");
-    setTodos((prevState) => [...prevState, { id: uuidv4, name: todoName }]);
-    console.log(uuidv4());
+    setTodos((prevState) => [...prevState, { id: uuidv4(), name: todoName }]);
+    // console.log(uuidv4());
   };
 
   const deleteTodo = (id) => {
-    const findIndex = todos.findIndex((v) => v.id === id);
-    setTodos((prevState) => {
-      const tempArr = [...prevState];
-      tempArr.splice(findIndex, 1);
-      return tempArr;
-    });
+    // const findIndex = todos.findIndex((v) => v.id === id);
+    // setTodos((prevState) => {
+    //   const tempArr = [...prevState];
+    //   tempArr.splice(findIndex, 1);
+    //   return tempArr;
+    // });
+    const filterTodos = todos.filter((v) => v.id !== id);
+    setTodos(filterTodos);
   };
+
+  useEffect(() => {
+    try {
+      const parseTodos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(parseTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("todos 저장");
+    try {
+      const stringifyTodos = JSON.stringify(todos);
+      console.log(stringifyTodos);
+      localStorage.setItem("todos", stringifyTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [todos]);
 
   return (
     <S.Container>
