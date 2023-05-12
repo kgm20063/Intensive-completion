89d39eaaff02e5658b1391b8621cc241d92652e0
kgm20063 <kgm20063@gmail.com>
2023-05-12 14:24:18 +0900

    FEAT: 선택 이벤트 추가

--- 2023.05.08/react-study/src/components/Todo/index.jsx
+++ 2023.05.08/react-study/src/components/Todo/index.jsx
@@ -1,12 +1,14 @@
 import Button from "../Button";
+import Checkbox from "../Checkbox";
 
 import * as S from "./styled";
 
 function Todo(props) {
-  const { deleteTodo } = props;
+  const { deleteTodo, handleSelected } = props;
 
   return (
     <S.Container>
+      <Checkbox onChange={handleSelected} />
       <S.Title>{props.children}</S.Title>
       <S.Wrap>
         <Button title="삭제" onClick={deleteTodo}>
