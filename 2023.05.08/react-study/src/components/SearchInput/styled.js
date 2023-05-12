c6ba2ff99c20e0b8b3361105cedfbbbf78f5f23c
kgm20063 <kgm20063@gmail.com>
2023-05-12 10:21:06 +0900

    FEAT: SearchInput 스타일 추가

---
+++ 2023.05.08/react-study/src/components/SearchInput/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SearchInputBox = styled.div``;
+
+export const SearchInput = styled.input`
+  width: 300px;
+  height: 30px;
+  padding: 10px;
+`;
