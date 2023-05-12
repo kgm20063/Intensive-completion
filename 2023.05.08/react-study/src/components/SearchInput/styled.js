3d1d106fef40e5e1c0f2b9e07b3214d90075fab7
kgm20063 <kgm20063@gmail.com>
2023-05-12 14:23:54 +0900

    FEAT: 검색 입력창 스타일 추가

--- 2023.05.08/react-study/src/components/SearchInput/styled.js
+++ 2023.05.08/react-study/src/components/SearchInput/styled.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
-export const SearchInputBox = styled.div``;
+export const SearchInputBox = styled.div`
+  display: flex;
+  gap: 4px;
+  width: 100%;
+`;
 
 export const SearchInput = styled.input`
-  width: 300px;
-  height: 30px;
-  padding: 10px;
+  flex-shrink: 1;
+  width: 100%;
+  padding: 8px 4px;
+  border: none;
+  border-bottom: 1px solid #eee;
+  outline: none;
+  &:focus {
+    border-color: #b3b3b3;
+    transition: 0.3s;
+  }
 `;
