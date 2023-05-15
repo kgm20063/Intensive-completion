3dc70c604e7376147b0ac457edea4697afbb6a65
kgm20063 <kgm20063@gmail.com>
2023-05-15 10:45:51 +0900

    FEAT: reducer 생성

---
+++ 2023.05.08/react-study/src/reduce/todos.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// reducer
+const todosSlice = createSlice({
+  // reducer name
+  name: todos,
+  // reducer의 초기값
+  initialState: [],
+  // reducer의 action들
+  reducers: {
+    createTodo: (state, action) => {},
+    deleteTodo: (state, action) => {},
+    deleteSelectedTodos: (state, action) => {},
+  },
+});
+
+export const { createTodo, deleteTodo, deleteSelectedTodos } = todosSlice.actions;
+export default todosSlice.reducer;
