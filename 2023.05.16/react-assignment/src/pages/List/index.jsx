f2596726a9de44ab89f16d2c18e4b2aebd44d3b1
kgm20063 <kgm20063@gmail.com>
2023-05-17 10:48:16 +0900

    FEAT: 정렬 완성

--- 2023.05.16/react-assignment/src/pages/List/index.jsx
+++ 2023.05.16/react-assignment/src/pages/List/index.jsx
@@ -6,12 +6,46 @@ import { memberList } from "../../constants/member-list";
 
 function List() {
   const [list, setList] = useState(memberList);
+
+  const sortAscById = () => {
+    setList((prevState) => {
+      return [...prevState].sort((a, b) => a.id - b.id);
+    });
+  };
+  const sortDescById = () => {
+    setList((prevState) => {
+      return [...prevState].sort((a, b) => b.id - a.id);
+    });
+  };
+
+  const sortAscByName = () => {
+    setList((prevState) => {
+      return [...prevState].sort((a, b) => (b.name < a.name ? 1 : -1));
+    });
+  };
+  const sortDescByName = () => {
+    setList((prevState) => {
+      return [...prevState].sort((a, b) => (a.name < b.name ? 1 : -1));
+    });
+  };
+  const sortAscByLevel = () => {
+    setList((prevState) => {
+      return [...prevState].sort((a, b) => {
+        if (a.level === b.level) return 0;
+        if (!a.level) return -1;
+        return b.level < a.level ? 1 : -1;
+      });
+    });
+  };
+
   return (
     <S.Container>
       <S.ButtonBox>
-        <S.Button>id정렬</S.Button>
-        <S.Button>name정렬</S.Button>
-        <S.Button>level정렬</S.Button>
+        <S.Button onClick={sortAscById}>id정렬</S.Button>
+        <S.Button onClick={sortDescById}>id내림차정렬</S.Button>
+        <S.Button onClick={sortAscByName}>name정렬</S.Button>
+        <S.Button onClick={sortDescByName}>name내림차정렬</S.Button>
+        <S.Button onClick={sortAscByLevel}>level정렬</S.Button>
       </S.ButtonBox>
       <S.ListBox>
         <S.ListItem>
