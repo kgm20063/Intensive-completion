d6d046cd2dbbcf899cc33c1d06d08619e6f0c6a0
kgm20063 <kgm20063@gmail.com>
2023-05-17 09:45:55 +0900

    FEAT: Router 생성

---
+++ 2023.05.16/react-assignment/src/pages/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import List from "./List";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<List />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
