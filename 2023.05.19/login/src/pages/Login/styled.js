baa7b37595cb58578ad373f872454547f8014b69
kgm20063 <kgm20063@gmail.com>
2023-05-19 15:50:06 +0900

    FEAT: login style 추가

---
+++ 2023.05.19/login/src/pages/Login/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: #95d094;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  //   justify-content: center;
+  align-items: center;
+  width: 600px;
+  height: 700px;
+  background: #fff;
+`;
+
+export const Title = styled.h1``;
+
+export const Input = styled.input`
+  padding: 10px;
+`;
+
+export const Button = styled.button``;
