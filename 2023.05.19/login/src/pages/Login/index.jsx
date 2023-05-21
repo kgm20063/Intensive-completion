0f13e702c01663e5629ab9e722f84fe29fbebc84
kgm20063 <kgm20063@gmail.com>
2023-05-21 18:51:39 +0900

    FEAT: 공백, 특수문자

--- 2023.05.19/login/src/pages/Login/index.jsx
+++ 2023.05.19/login/src/pages/Login/index.jsx
@@ -3,20 +3,31 @@ import { Link } from "react-router-dom";
 
 import * as S from "./styled";
 
-function Login({ name, id, password }) {
-  const [valueName, setValueName] = useState("");
-  const [valueId, setValueId] = useState("");
-  const [valuePw, setValuePw] = useState("");
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
+  const regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
+
+  function handleClick() {
+    if (userName !== "" && userId !== "" && userPw !== "") {
+      if (userName.match(regExp)) {
+        alert("특수문자 입력 불가능");
+      }
+    } else {
+      alert("입력 안된 곳 있음");
+    }
+  }
 
   return (
     <S.Container>
       <S.Form>
         <S.Title>로그인</S.Title>
-        <S.Input type="text" className="name" placeholder="이름" minlength="2" maxlength="5" value={name} />
-        <S.Input type="text" className="id" placeholder="아이디" minlength="4" value={id} />
-        <S.Input type="password" className="password" placeholder="비밀번호" minlength="8" value={password} />
-        <S.Button type="submit">
-          <Link to="/Main">로그인</Link>
+        <S.Input type="text" className="name" placeholder="이름" maxLength={5} minLength={2} onChange={setUserName} />
+        <S.Input type="text" className="id" placeholder="아이디" minLength={4} onChange={setUserId} />
+        <S.Input type="password" className="password" placeholder="비밀번호" minLength={8} onChange={setUserPw} />
+        <S.Button type="submit" onClick={handleClick}>
+          로그인
         </S.Button>
       </S.Form>
     </S.Container>
