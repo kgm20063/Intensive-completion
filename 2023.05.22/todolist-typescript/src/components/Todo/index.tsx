f05f292cb2c8a5da9689ded8c8bca83d531636fe
kgm20063 <kgm20063@gmail.com>
2023-05-25 14:28:41 +0900

    FEAT: Todo

--- 2023.05.22/todolist-typescript/src/components/Todo/index.tsx
+++ 2023.05.22/todolist-typescript/src/components/Todo/index.tsx
@@ -1,18 +1,38 @@
+import type { ChangeEvent } from 'react'
+
 import { Todo as TodoType } from "../../types/todo";
 
 import Button from "../Button";
+import Input from "../Input";
 
 interface TodoProps {
   name: TodoType["name"];
+  isEdited: boolean;
   deleteTodo: () => void;
   handleSelected: (checked: boolean) => void;
+  toggleEditTodo: () => void;
+  setEditedName: (name: TodoType["name"]) => void;
+  editTodo: () => void;
 }
 
-function Todo({ name, deleteTodo, handleSelected }: TodoProps) {
+function Todo({
+  name,
+  isEdited,
+  deleteTodo,
+  handleSelected,
+  toggleEditTodo,
+  setEditedName,
+  editTodo,
+  }: TodoProps) {
+  const handleEditedName = ({ target: { value }, }: ChangeEvent<HTMLInputElement>) => {
+    setEditedName(value);
+  }
   return (
     <li>
-      <span>{name}</span>
+      {isEdited ? <Input defaultValue={name} onChange={handleEditedName}/> : <span>{name}</span>}
       <Button onClick={deleteTodo}>삭제</Button>
+      <Button onClick={toggleEditTodo}>{isEdited ? "취소" : "수정"}</Button>
+      {isEdited && <Button onClick={editTodo}>저장</Button>}
     </li>
   );
 }
