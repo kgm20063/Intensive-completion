4f97158b2004c6a9e05587b4b8a6c00994624e4e
kgm20063 <kgm20063@gmail.com>
2023-05-25 14:28:52 +0900

    FEAT: TodoList

--- 2023.05.22/todolist-typescript/src/components/TodoList/index.tsx
+++ 2023.05.22/todolist-typescript/src/components/TodoList/index.tsx
@@ -7,28 +7,59 @@ interface TodoListProps {
   todos: TodoType[];
   searchValue: string;
   deleteTodo: (id: TodoType["id"]) => void;
+  editedTodoId: TodoType['id'] | undefined;
   setSelectedTodoIds: DispatchSetStateAction<TodoType["id"][]>;
+  toggleEditTodo: (id: string) => void;
+  setEditedName: (name: TodoType['name']) => void
+  editTodo: () => void;
 }
 
-function TodoList({ todos, searchValue, deleteTodo, setSelectedTodoIds }: TodoListProps) {
+function TodoList({
+  todos,
+  searchValue,
+  deleteTodo,
+  setSelectedTodoIds,
+  toggleEditTodo,
+  editedTodoId,
+  setEditedName,
+  editTodo,
+}: TodoListProps) {
   return (
     <ul>
-      {todos.map(({ id, name }) => {
-        const handleDeleteTodo = () => {
-          deleteTodo(id);
-        };
+      {todos
+        .filter(({ name }) => name.includes(searchValue))
+        .map(({ id, name }) => {
+          const handleDeleteTodo = () => {
+            deleteTodo(id);
+          };
 
-        const handleSelected = (checked: boolean) => {
-          if (checked) {
-            setSelectedTodoIds((prevState) => [...prevState, id]);
-          } else {
-            setSelectedTodoIds((prevState) => prevState.filter((prevId) => prevId !== id));
+          const handleSelected = (checked: boolean) => {
+            if (checked) {
+              setSelectedTodoIds((prevState) => [...prevState, id]);
+            } else {
+              setSelectedTodoIds((prevState) => prevState.filter((prevId) => prevId !== id));
+            }
+          };
+
+          const handleEditTodo = () => {
+            toggleEditTodo(id);
           }
-          console.log(id, name);
-        };
 
-        return <Todo key={id} name={name} deleteTodo={handleDeleteTodo} handleSelected={handleSelected} />;
-      })}
+          const isEdited = editedTodoId === id;
+
+          return (
+            <Todo
+              key={id}
+              name={name}
+              isEdited={isEdited}
+              deleteTodo={handleDeleteTodo}
+              handleSelected={handleSelected}
+              toggleEditTodo={handleEditTodo}
+              setEditedName={setEditedName}
+              editTodo={editTodo}
+            />
+          );
+        })}
     </ul>
   );
 }
