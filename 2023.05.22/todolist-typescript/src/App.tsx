fd48cd11836b9bf0144e672397b38d09e432b9c9
kgm20063 <kgm20063@gmail.com>
2023-05-25 14:28:22 +0900

    FEAT: App

--- 2023.05.22/todolist-typescript/src/App.tsx
+++ 2023.05.22/todolist-typescript/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import type { ChangeEvent } from "react";
+import type { ChangeEvent, KeyboardEvent } from "react";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -13,6 +13,8 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [searchValue, setSearchValue] = useState<string>("");
   const [selectedTodoIds, setSelectedTodoIds] = useState<Todo["id"][]>([]);
+  const [editedTodoId, setEditedTodoId] = useState<Todo['id'] | undefined>();
+  const [editedName, setEditedName] = useState<Todo['name']>("");
 
   const handleTodoName = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
     setTodoName(value);
@@ -29,21 +31,52 @@ function App() {
     });
   };
 
+  const handleEnterPress = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
   const addTodo = () => {
     setTodos((prevState) => [...prevState, { id: uuidv4(), name: todoName }]);
     setTodoName("");
   };
 
+  const toggleEditTodo = (id: Todo['id']) => {
+    setEditedTodoId(prevState => (prevState === id ? undefined : id));
+  };
+
+  const handleEditedName = (name: Todo['name']) => {
+    setEditedName(name)
+  }
+
+  const editTodo = () => {
+    setTodos(prevState => {
+      return prevState.map(({ id, name }) => {
+        if (id === editedTodoId) {
+          return { id, name: editedName }
+        }
+        return { id, name };
+      });
+    });
+    setEditedTodoId(undefined)
+    setEditedName("")
+  };
+
   return (
     <div className="App">
-      <Input value={todoName} onChange={handleTodoName} />
+      <Input value={todoName} onChange={handleTodoName} onKeyUp={handleEnterPress} />
       <button onClick={addTodo}>Todo 추가</button>
       <Input onChange={handleSearchValue} />
       <TodoList
         todos={todos}
         searchValue={searchValue}
+        editedTodoId={editedTodoId}
         deleteTodo={handleDeleteTodo}
         setSelectedTodoIds={setSelectedTodoIds}
+        toggleEditTodo={toggleEditTodo}
+        setEditedName={handleEditedName}
+        editTodo={editTodo}
       />
     </div>
   );
